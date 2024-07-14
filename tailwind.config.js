ac10c355d2a72b9eaa211d4e9e07b4c199240042
Sanilal P Sathyan <sanilal@gmail.com>
2024-07-14 15:29:15 +0400

    tailwind added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightYellow: "#f6d876",
+        white2: "#fffbf2",
+        darkGreen: "#05ab54",
+        dark: "#1e1e1e",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        leagueGothic: ["League Gothic", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        }
+      }
+    },
   },
   plugins: [],
 }
