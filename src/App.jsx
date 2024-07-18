553a2c514b6899ad1dab4d5481036982fb2cc7c8
Sanilal P Sathyan <sanilal@gmail.com>
2024-07-18 09:51:38 +0400

    hero updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ import Hero from './components/Hero/Hero';
 const App = () => {
   return (
     <div className='overflow-x-hidden bg-white2'>
+     <div className="relative overflow-hidden">
       <Navbar />
       <Hero />
+     </div>
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,11 +8,11 @@ import { motion } from 'framer-motion';
 
 const SlideUp = (delay) => {
     return {
-        initial: {
+        hidden: {
             y: "100%",
             opacity: 0,
         },
-        animate: {
+        show: {
             y: 0,
             opacity: 1,
             transition: {
@@ -26,29 +26,29 @@ const SlideUp = (delay) => {
 const Hero = () => {
   return (
     <main>
-        <div className="container min-h-[600px] flex justify-center">
+        <div className="container min-h-[600px] flex justify-center relative z-10">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-24 place-items-center justify-between">
                 {/* text content here */}
                 <div className='space-y-3 mt-14 to-current md:text-left md:mt-0'>
                     <motion.h1 variants={SlideUp(0.5)} 
-                    initial="initial"
-                    whileInView="animate"
+                    initial="hidden"
+                    whileInView="show"
                     className='relative text-5xl lg:text-7xl xl:text-8xl font-bold uppercase text-outline text-transparent'>Yummy
                         <img src={Leaf} alt="" className='absolute w-[50px] top-0 right-0 md:right-[100px]' />
                     </motion.h1>
                     <motion.h1 
                     variants={SlideUp(1)}
-                    initial="initial"
-                    whileInView="animate"
+                    initial="hidden"
+                    whileInView="show"
                     className='text-5xl lg:text-7xl xl:text-8xl font-bold uppercase'>BREAKFAST</motion.h1>
                     <motion.p 
                     variants={SlideUp(1.5)}
-                    initial="initial"
-                    whileInView="animate"                    className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit possimus dolores quisquam doloribus tenetur eaque saepe placeat quidem vitae repellendus aliquid nulla, exercitationem pariatur. Incidunt at nam excepturi repellat officiis! </motion.p>
+                    initial="hidden"
+                    whileInView="show"                    className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit possimus dolores quisquam doloribus tenetur eaque saepe placeat quidem vitae repellendus aliquid nulla, exercitationem pariatur. Incidunt at nam excepturi repellat officiis! </motion.p>
                     <motion.button
                     variants={SlideUp(2)}
-                    initial="initial"
-                    whileInView="animate"
+                    initial="hidden"
+                    whileInView="show"
                     className='btn-primary inline-block !mt-10'>
                         <IoCartOutline />
                         Order Now
@@ -56,13 +56,30 @@ const Hero = () => {
                 </div>
                 {/* Images here */}
                 <div className='relative'>
-                    <img src={FoodPng} alt="" className='w-[450px] img-shadow' />
-                    <img src={Spoon} alt="" className='w-[350px] absolute bottom-[-120px] -left-16 rotate-[75deg] img-shadow' />
-                    <img src={Banana} alt="" className='w-[400px]  absolute  top-[-30px] right-[-130px] md:right-[-160px] img-shadow' />
+                    <motion.img 
+                    initial={{ opacity: 0, rotate: 20, x: 200, y: 100 }}
+                    whileInView={{ opacity:1, rotate: 0, x: 0, y: 0 }}
+                    transition={{ duration: 0.8 }}
+                    src={FoodPng} alt="" className='w-[450px] img-shadow' />
+                    <motion.img 
+                    initial={{ opacity: 0, rotate: 120, x: 200, y: 100 }}
+                    whileInView={{ opacity:1, rotate: 75, x: 0, y: 0 }}
+                    transition={{ duration: 0.8 }}
+                    src={Spoon} alt="" className='w-[350px] absolute bottom-[-120px] -left-16 rotate-[75deg] img-shadow' />
+                    <motion.img 
+                     initial={{ opacity: 0, rotate: 20, x: 200, y: 100 }}
+                     whileInView={{ opacity:1, rotate: 0, x: 0, y: 0 }}
+                     transition={{ duration: 0.8 }}
+                    src={Banana} alt="" className='w-[400px]  absolute  top-[-30px] right-[-130px] md:right-[-160px] img-shadow' />
                 </div>
                 {/* text content here */}
             </div>
         </div>
+        {/* background Yellow */}
+        <motion.div 
+        initial={{ opacity:0, rotate: 60, x: 200, y: 100 }}
+        whileInView={{ opacity:1, rotate: 40, x: 0, y: 0 }}
+        className='w-[2500px] h-[2500px] rounded-3xl bg-yellow-300 absolute top-[-30%] left-[-70%] z-0'></motion.div>
     </main>
   )
 }
