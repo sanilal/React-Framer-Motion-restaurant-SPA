b23ef2b1347a61dc652a8ec82bd9dce4dde2573c
Sanilal P Sathyan <sanilal@gmail.com>
2024-07-19 09:22:42 +0400

    Hot Dessert

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import HotDessert from './components/HotDessert/HotDessert';
 
 
 
 const App = () => {
   return (
-    <div className='overflow-x-hidden bg-white2'>
+    <div className='overflow-x-hidden bg-white2 text-dark'>
      <div className="relative overflow-hidden">
       <Navbar />
       <Hero />
      </div>
+      <HotDessert />
     </div>
   )
 }
--- src/components/HotDessert/HotDessert.jsx
+++ src/components/HotDessert/HotDessert.jsx
@@ -33,7 +33,33 @@ const HotDEssertData = [
 const HotDessert = () => {
   return (
     <section>
-        
+        <div className="container py-12">
+            <motion.h3
+            variants={SlideUp(0)}
+            initial="hidden"
+            whileInView="show"
+            className="text-2xl font-semibold text-green-800 uppercase py-8">Hot Dessert</motion.h3>
+
+            {/* grid section */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                {HotDEssertData.map((item)=>{
+                    return (
+                        <motion.div 
+                        variants={SlideUp(item.delay)}
+                        initial="hidden"
+                        whileInView="show"
+                        className='group bg-white/50 shadow-md p3 flex items-center gap-3'>
+                            <img src={item.img} alt="" className='w-24 rounded-full img-shadow group-hover:scale-125 transition-all duration-700 group-hover:rotate-[50deg]' />
+                            <div>
+                                <h3 className='text-xl font-semibold'>{item.name}</h3>
+                                <p className='text-xl text-yellow-500'>{item.price}</p>
+                            </div>
+                        </motion.div>
+                    )
+                })}
+
+            </div>
+        </div>
     </section>
   )
 }
